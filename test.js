69bd211ee295582bb9d2e9b5d1f4a2b08efbd65c
hammad38 <hammadsami119@gmail.com>
2023-03-14 13:12:04 +0500

    .

---
+++ test.js
@@ -0,0 +1,15 @@
+// const greet = (name) => {
+//   console.log(`Hello ${name}`);
+// };
+
+// greet("Hammad");
+// greet("Shoaib");
+
+// console.log(global)
+
+// setTimeout(() => {
+//   console.log("set timeout");
+// }, 3000);
+
+console.log(__dirname);
+console.log(__filename);
